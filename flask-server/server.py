f1083a7c3c82ed82bf637cb9c03c3147a6f24d9b
Mahit Verma <mahitverma@gmail.com>
2024-09-11 13:45:48 -0400

    tryna get transcript from yt vid
    code to get trans, getting a not found url error :D

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+from youtube_transcript_api import YouTubeTranscriptApi
 
 app = Flask(__name__)
 
-#Members API Routes
+# Route to get the transcript of a YouTube video
+@app.route('https://www.youtube.com/watch?v=YUnIuodiOV4')
+def get_transcript(video_id):
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        return jsonify(transcript)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+# Members API Routes
 @app.route('/members')
 def members():
     return {"members": ["member1", "member2", "member3"]}
